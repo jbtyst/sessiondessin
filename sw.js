6036f0f9f100cebb2793ec9064dbcd442b927d1c
jbtyst <68372601+jbtyst@users.noreply.github.com>
2025-12-24 14:02:06 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'session-dessin-v4';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.49/Tone.js',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Kalam:wght@700&display=swap'
+];
+
+// Installation du Service Worker et mise en cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Récupération des ressources (Offline First)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
